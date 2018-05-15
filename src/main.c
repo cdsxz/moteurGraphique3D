565448a7bc55428c3efa420750f95a9099904901
cdsxz <clement.mer37@gmail.com>
2018-05-15 14:40:18 +0200

    First commit

---
+++ src/main.c
@@ -0,0 +1,41 @@
+﻿#include "window.h"
+#include "events.h"
+#include "vector.h"
+#include "geometry.h"
+#include "model.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int main( int argc, char ** argv ) {
+
+	const int width		= 1024;
+	const int height	= 768;
+	
+	// Ouverture d'une nouvelle fenêtre
+	window_t * mainwindow = WindowInit( width, height, 4 );
+
+	int done = false;
+	
+	// Tant que l'utilisateur de ferme pas la fenêtre
+	while ( !done ) {	
+		
+		// Mise à jour et traitement des evênements de la fenêtre
+		done = EventsUpdate( mainwindow );
+		
+		// Effacement de l'écran avec une couleur
+		WindowDrawClearColor( mainwindow, 64, 64, 64 );
+		
+		// Dessin d'un point blanc au milieu de le fenêtre
+		WindowDrawPoint( mainwindow, width / 2, height / 2, 255, 255, 255 );
+
+		// Mise à jour de la fenêtre
+		WindowUpdate( mainwindow );
+
+	}
+	
+	// Fermeture de la fenêtre
+	WindowDestroy( mainwindow );
+
+	return 1;
+}
+
---
+++ src/model.c
@@ -0,0 +1,48 @@
+#include "model.h"
+
+vector_t * g_vertex;
+vector_t * g_norm;
+vector_t * g_texcoord;
+vector_t * g_face;
+
+vector_t * ModelVertices() {
+	return g_vertex;
+}
+
+vector_t * ModelNormals() {
+	return g_norm;
+}
+
+vector_t * ModelTexcoords() {
+	return g_texcoord;
+}
+
+vector_t * ModelFaces() {
+	return g_face;
+}
+
+vec3f_t ModelGetVertex( int index ) {
+	vec3f_t v = *(vec3f_t*)VectorGetFromIdx( ModelVertices(), index );
+	return v;
+}
+
+vec3f_t ModelGetNormal( int index ) {
+	vec3f_t t = *(vec3f_t*)VectorGetFromIdx( ModelNormals(), index );
+	return t;
+}
+
+vec3f_t ModelGetTexcoord( int index ) {
+	vec3f_t t = *(vec3f_t*)VectorGetFromIdx( ModelTexcoords(), index );
+	return t;
+}
+
+face_t ModelGetFace( int index ) {
+	face_t f = *(face_t*)VectorGetFromIdx( ModelFaces(), index );
+	return f;
+}
+
+bool ModelLoad( char * objfilename ) {
+	// Fonction à implementer
+	return true;
+}
+
